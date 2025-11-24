0da8e5753720f351cf6f9bfae6432fddc0247a91
sel-hasn <fangyuansamach@gmail.com>
2025-11-24 12:39:31 +0100

    adding login rout schema and and start the handler

--- app/routes/users.ts
+++ app/routes/users.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 import { hashPassword, verifyPassword } from '../utils/password';
 import { RegisterSchema, LoginSchema, RegisterSchemaType, LoginSchemaType } from '../schemas/auth.schemas'
+import { request } from 'http';
 
 export async function userRouts(fastify: FastifyInstance) {
     // Creat new user
@@ -68,7 +69,9 @@ export async function userRouts(fastify: FastifyInstance) {
 
                 const passwordHash = await hashPassword(password);
 
-                const result = fastify.db.prepare(`INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`).run(username, email, passwordHash);
+                const result = fastify.db.prepare(
+                    `INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`
+                ).run(username, email, passwordHash);
 
                 const userId = result.lastInsertRowid as number;
 
@@ -82,9 +85,65 @@ export async function userRouts(fastify: FastifyInstance) {
                 })
             } catch (err) {
                 fastify.log.error(err);
-                return reply.code(500).send({ error: 'Internal server error' });
+                return reply.code(500).send({
+                    error: 'Internal server error'
+                });
             }
         }
 
+    });
+    fastify.route({
+        method: 'POST',
+        url: '/api/auth/login',
+        schema: {
+            body: LoginSchema,
+            response: {
+                200: {
+                    description: 'Login successful',
+                    type: 'object',
+                    properties: {
+                        message: {type: 'string' },
+                        user: {
+                            type: 'object',
+                            properties: {
+                                id: { type: 'number' },
+                                username: { type: 'string' },
+                                email: { type: 'string' }
+                            }
+                        }
+                    }
+                },
+                400: {
+                    description: 'Bad request',
+                    type: 'object',
+                    properties: {
+                        error: { type: 'string' }
+                    }
+                },
+                401: {
+                    description: 'Unauthorized - invalid credentials',
+                    type: 'object',
+                    properties: {
+                        error: { type: 'string' }
+                    }
+                }
+            }
+        },
+        handler: async (request: FastifyRequest<{ Body: LoginSchemaType }>, reply: FastifyReply) => {
+            try {
+                const { identifier, password } = request.body;
+                
+                const user = fastify.db.prepare(
+                    'SELECT id, username, email, password_hash FROM users WHERE email = ? OR username = ?'
+                ).get(identifier, identifier) as {
+                    id: number,
+                    username: string,
+                    email: string,
+                    password_hash: string
+                } | undefined;
+
+                
+            }
+        }
     })
 }
